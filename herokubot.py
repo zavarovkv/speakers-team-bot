0ef1ae1ed30fdd16e4d4deeb07fd570cc84c5142
Konstantin Zavarov <zavarovkv@gmail.com>
2020-08-21 18:48:00 +0300

    fix

--- herokubot.py
+++ herokubot.py
@@ -41,6 +41,8 @@ def start(update, context):
 
 
 def start2(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    
     buttons = [[
         InlineKeyboardButton('Я спикер', callback_data=str(IAM_SPEAKER))
     ], [
