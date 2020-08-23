5ba073310e98d900ee27c54681d8637958f4d09d
Konstantin Zavarov <zavarovkv@gmail.com>
2020-08-23 18:07:17 +0300

    fix

--- herokubot.py
+++ herokubot.py
@@ -91,7 +91,7 @@ def change_button_type(btn_code, context):
 def track_type(t_type, context):
     if t_type == const.TRACK_ENGINEERING:
         for val in const.TRACK_ENGINEERING_SET:
-            if val in context.user_data
+            if val in context.user_data:
                 if context.user_data[val] is True:
                     return '▣ '
     return '☐ '
